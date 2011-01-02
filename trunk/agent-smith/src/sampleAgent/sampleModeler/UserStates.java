4e9911d26588d46ad36ff717716f4dd921092bc4
sekely <sekely@452fa59e-82f9-6693-3c50-295c68f6b78a>
2011-01-02 20:49:19 +0000

    updating initial prob table

--- trunk/agent-smith/src/sampleAgent/sampleModeler/UserStates.java
+++ trunk/agent-smith/src/sampleAgent/sampleModeler/UserStates.java
@@ -8,19 +8,48 @@ public class UserStates {
 	public UserStates(){
 		int matSize = EnumUserState.values().length;
 		this.prob = new double[matSize][matSize];
-		setCell(0.10, EnumUserState.F0, EnumUserState.NS);
-		setCell(0.70, EnumUserState.F0, EnumUserState.F0);
-		setCell(0.20, EnumUserState.F0, EnumUserState.F1);
-		setCell(0.00, EnumUserState.F0, EnumUserState.TR);
 		
-		setCell(0.10, EnumUserState.F1, EnumUserState.NS);
-		setCell(0.70, EnumUserState.F1, EnumUserState.F1);
-		setCell(0.20, EnumUserState.F1, EnumUserState.F2);
-		setCell(0.00, EnumUserState.F1, EnumUserState.TR);
+		setCell(EnumUserState.NS, EnumUserState.NS, 0.99);
+		setCell(EnumUserState.NS, EnumUserState.IS, 0.01);
+		setCell(EnumUserState.NS, EnumUserState.F0, 0.00);
+		setCell(EnumUserState.NS, EnumUserState.F1, 0.00);
+		setCell(EnumUserState.NS, EnumUserState.F2, 0.00);
+		setCell(EnumUserState.NS, EnumUserState.TR, 0.00);
 		
-		setCell(0.10, EnumUserState.F2, EnumUserState.NS);
-		setCell(0.90, EnumUserState.F2, EnumUserState.F2);
-		setCell(0.00, EnumUserState.F2, EnumUserState.TR);
+		setCell(EnumUserState.IS, EnumUserState.NS, 0.05);
+		setCell(EnumUserState.IS, EnumUserState.IS, 0.20);
+		setCell(EnumUserState.IS, EnumUserState.F0, 0.60);
+		setCell(EnumUserState.IS, EnumUserState.F1, 0.10);
+		setCell(EnumUserState.IS, EnumUserState.F2, 0.05);
+		setCell(EnumUserState.IS, EnumUserState.TR, 0.00);
+		
+		setCell(EnumUserState.F0, EnumUserState.NS, 0.10);
+		setCell(EnumUserState.F0, EnumUserState.IS, 0.00);
+		setCell(EnumUserState.F0, EnumUserState.F0, 0.70);
+		setCell(EnumUserState.F0, EnumUserState.F1, 0.20);
+		setCell(EnumUserState.F0, EnumUserState.F2, 0.00);
+		setCell(EnumUserState.F0, EnumUserState.TR, 0.00);
+		
+		setCell(EnumUserState.F1, EnumUserState.NS, 0.10);
+		setCell(EnumUserState.F1, EnumUserState.IS, 0.00);
+		setCell(EnumUserState.F1, EnumUserState.F0, 0.00);
+		setCell(EnumUserState.F1, EnumUserState.F1, 0.70);
+		setCell(EnumUserState.F1, EnumUserState.F2, 0.20);
+		setCell(EnumUserState.F1, EnumUserState.TR, 0.00);	
+		
+		setCell(EnumUserState.F2, EnumUserState.NS, 0.10);
+		setCell(EnumUserState.F2, EnumUserState.IS, 0.00);
+		setCell(EnumUserState.F2, EnumUserState.F0, 0.00);
+		setCell(EnumUserState.F2, EnumUserState.F1, 0.00);
+		setCell(EnumUserState.F2, EnumUserState.F2, 0.90);
+		setCell(EnumUserState.F2, EnumUserState.TR, 0.00);
+		
+		setCell(EnumUserState.TR, EnumUserState.NS, 0.80);
+		setCell(EnumUserState.TR, EnumUserState.IS, 0.00);
+		setCell(EnumUserState.TR, EnumUserState.F0, 0.00);
+		setCell(EnumUserState.TR, EnumUserState.F1, 0.00);
+		setCell(EnumUserState.TR, EnumUserState.F2, 0.00);
+		setCell(EnumUserState.TR, EnumUserState.TR, 0.20);
 	}
 	
 	public UserStates(double[][] prob) throws ArrayIndexOutOfBoundsException{
@@ -33,7 +62,7 @@ public class UserStates {
 		}
 	}
 	
-	public void setCell (double value, EnumUserState from, EnumUserState to){
+	public void setCell (EnumUserState from, EnumUserState to, double value){
 		prob[from.value()][to.value()] = value;
 	}
 	
